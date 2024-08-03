d4dfb1a29f8b06369a5990ee34d993c9282f84ae
amplitud3 <111232829+amplitud3@users.noreply.github.com>
2024-08-03 13:56:41 +0530

    loop added

--- main.cpp
+++ main.cpp
@@ -2,7 +2,9 @@
 using namespace std;
 
 void qwsh_loop();
-
+int qwsh_execute(char **args);
+char **qwsh_split_line(char *line);
+char *qwsh_read_line(void);	
 int main()
 {
 
@@ -11,12 +13,56 @@ return 0;
 
 }
 
+
+/*
+ *Basic loop of a shell 
+ *Read
+ *Parse
+ *Execute
+ */
+
+
 void qwsh_loop()
 {
+	char *line;
+	char **args;
+	int status;
 
 
+
+	do{
+	   printf("> ");
+   	   line = qwsh_read_line();
+	   args = qwsh_split_line(line);
+           status = qwsh_execute(args);
+
+
+	  free(line);
+	  free(args);
+	  }while(status);	  
 
 }
 
 
+char *qwsh_read_line()
+{
+
+
+return 0;
+}
+
+
+char  **qwsh_split_line(char *line)
+{
+
+
+return 0;
+}
+
+
+int qwsh_execute()
+{
+
+return 0;
+}
 
